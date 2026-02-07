d10843d7a5f540267818752750cc2dd15deb5fc9
Lawrence The Cinarian <cinarilabs@gmail.com>
2026-02-07 15:25:54 +0100

    Create atms.c

---
+++ atms.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+/*Can't Store Info on previous use but I'll update it as soon as i get the gist of it
+Still Learning Fundamentals though and will keep learning*/
+int main() {
+do{
+  int bal-USD = 200000;
+  int bal-NGN = 200000;
+  char atmp [70]; //ATM PASSWORD
+printf("Welcome To Automated Decentralized Mines");
+printf("Please Enter You Password: ");
+  scanf("%69s", atmp);
+   if(strcmp(atmp, "Lawxey8") == 0) {
+     printf("Access Granted\n");
+     printf("Menu\n");
+     printf("Balance (USD and NGN)\n");
+     printf("\n");
+     printf("\n");
+   }
+  else {
+    printf("Access Denied");
+    return 0;
+  }
+}
+while(bal != 0);
+  return 0;
+}
+
